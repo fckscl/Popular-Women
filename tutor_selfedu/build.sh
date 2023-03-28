0e781cbecba37b82c9580a8037166d16f85262f0
fckscl <dimapugach31102003@gmail.com>
2023-03-28 17:31:25 +0300

    duild lf

--- tutor_selfedu/build.sh
+++ tutor_selfedu/build.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
-
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 - --uninstall
 curl -sSL https://install.python-poetry.org | python3 -
 poetry self update
 poetry install
-
 python manage.py collectstatic --no-input
 python manage.py migrate
