edfb9d73dfe1ccbde1a0c028598f2e765c8df45b
fckscl <dimapugach31102003@gmail.com>
2023-03-28 17:02:05 +0300

    reinstall poetry

--- tutor_selfedu/build.sh
+++ tutor_selfedu/build.sh
@@ -2,6 +2,8 @@
 # exit on error
 set -o errexit
 
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 - --uninstall
+curl -sSL https://install.python-poetry.org | python3 -
 poetry self update
 poetry install
 
